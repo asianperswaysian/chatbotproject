fbd2c6366a36a9f11cd9c3c6ba30a0d3390e4032
Niko Vergara <asianperswaysian.13@gmail.com>
2016-10-12 08:26:04 -0700

    Im kinda done not really ig not

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,18 @@
+var chatAnswer;
+
 function speak() {
    $("#chat-area").text($("#userInput").val());
+};
+function myDivArea() {
+  $("#myDivArea").prepend("This is the beginning!");
+  $("#myDivArea").append("And finally, the end!");
+}
+
+var questions {
+  "What it do shawty":["Nothing much", "What does shawty mean", "I aint your boo thang"],
+  "what the time Kylie?": [hours + ":" + minutes],
+  "but like how old are you?": ["old enough", "I was here before Harry Potter killed Voldemort the seconnd time", "never ask a girl that"],
+  "Whats your name?": ["My name Kylie"],
+  "do you like animals": ["it varies", "i like turtles", "I prefer fishies more"],
+  "Do you have a mother": ["Idk bro", "I'm robot you human", "Error dude"]
 }
