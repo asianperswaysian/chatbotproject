bf490a5627abb0a36e50e5f6bdaabacaf82dc643
Niko Vergara <asianperswaysian.13@gmail.com>
2016-10-12 08:39:36 -0700

    I finished i think

--- chatbot.js
+++ chatbot.js
@@ -9,10 +9,13 @@ function myDivArea() {
 }
 
 var questions {
-  "What it do shawty":["Nothing much", "What does shawty mean", "I aint your boo thang"],
+  "What it do shawty":["Nothing much", "What does shawty mean", "What?"],
   "what the time Kylie?": [hours + ":" + minutes],
-  "but like how old are you?": ["old enough", "I was here before Harry Potter killed Voldemort the seconnd time", "never ask a girl that"],
+  "but like how old are you?": ["old enough", "I was here before Harry Potter killed Voldemort the second time", "never ask a girl that"],
   "Whats your name?": ["My name Kylie"],
   "do you like animals": ["it varies", "i like turtles", "I prefer fishies more"],
-  "Do you have a mother": ["Idk bro", "I'm robot you human", "Error dude"]
+  "Do you have a mother": ["Idk bro", "I'm a robot you human", "Error dude"],
+  "I like you":["WOW a little to soon for questions like that", "I really dont", "I'm asexual and nonliving", "thanks bud"],
+  "Black or white";["I'm colorblind","White and gold"]
+
 }
