ceec0f7ca9e4f29d267dbc656987539f12762b2f
Niko Vergara <asianperswaysian.13@gmail.com>
2016-12-07 08:01:53 -0800

    finished chatbot again

--- chatbot.js
+++ chatbot.js
@@ -1,21 +1,50 @@
-var chatAnswer;
 
-function speak() {
-   $("#chat-area").text($("#userInput").val());
-};
+
+var fallBack = ["idk lol",  "you should know", "kk cool"]
+
+function returnTime() {
+  var now = new Date(Date.now());
+  return now;
+}
+
 function myDivArea() {
   $("#myDivArea").prepend("This is the beginning!");
   $("#myDivArea").append("And finally, the end!");
 }
 
-var questions {
-  "What it do shawty":["Nothing much", "What does shawty mean", "What?"],
-  "what the time Kylie?": [hours + ":" + minutes],
-  "but like how old are you?": ["old enough", "I was here before Harry Potter killed Voldemort the second time", "never ask a girl that"],
-  "Whats your name?": ["My name Kylie"],
-  "do you like animals": ["it varies", "i like turtles", "I prefer fishies more"],
-  "Do you have a mother": ["Idk bro", "I'm a robot you human", "Error dude"],
-  "I like you":["WOW a little to soon for questions like that", "I really dont", "I'm asexual and nonliving", "thanks bud"],
-  "Black or white";["I'm colorblind","White and gold"]
+var responses = {
+  "what's your name?": "My name Kylie",
+  "how old are you?": "I am ageless",
+  "what time is it": "It is currently " + returnTime() + ".",
+  "how are you": "I am well, thanks",
+  "what's your favorite sport": "silly boy I'm too lazy for all that jazz",
+  "wheres your parents": "well Steven jobs died so",
+  "i like you": "oh thats nice",
+  "how many eggs in the box Carol?": "like 42 idk i havent counted yet",
+  "How are you feeling right now": "im feeling #blessed",
+  "whats your favorite music": "rnb for life",
+  "whats your favorite color": "white and gold son",
 
 }
+
+function speak() {
+  // $("#chat-area").text($("#userInput").val()); <br>
+  var question = document.getElementById("userInput").value
+  question = question.toLowerCase()
+  var answer = responses[question]
+
+  if(answer == undefined) {
+    var rand = Math.floor((Math.random() * fallBack.length));
+    answer = fallBack[rand]
+  }
+
+   document.getElementById("chat-area").innerHTML += question + "<br>" + answer + "<br>"
+
+}
+
+document.addEventListener("keydown", function(e) {
+  if(e.keyCode == 13) {
+    speak()
+  }
+
+})
